bd3c4cdc89c3b74b2a1c4102a9f1275fc36e60eb
Vector1331 <89854207+Vector1331@users.noreply.github.com>
2022-03-24 16:23:02 +0900

    섹션1 - JPA와 DB 설정, 동작확인
    섹션1. 프로젝트 환경설정 완

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember=  memberRepository.find(saveId);
+
+        //then
+        assertThat(findMember.getId(),is(equalTo(member.getId())));
+        assertThat(findMember.getUsername(),is(equalTo(member.getUsername())));
+        // findMember == member 는 true이다. 같은 영속성 content안에선 같은 entity로 인식
+    }
+
+
+
+}
