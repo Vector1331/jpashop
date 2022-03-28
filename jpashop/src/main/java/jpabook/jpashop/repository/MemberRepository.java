1e085b7ea01ea64ef47a3e83653a76095237bdd9
Vector1331 <tpgus4796@gmail.com>
2022-03-28 19:53:41 +0900

    회원 도메인- 회원 레파지토리 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member); //jpa가 저장하는 로직 된다.
+    }
+
+    public Member findOne(Long id) {
+        return em. find(Member.class, id);
+    }
+
+    //createQuery은 Entity 객체를 대상으로 쿼리를 줘서, 테이블 단위로 쿼리 처리하는 sql과 조금 다름
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
