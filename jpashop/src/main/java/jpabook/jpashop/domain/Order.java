9930dc6832674071da6161b240dc48ebe9e2bc05
Vector1331 <tpgus4796@gmail.com>
2022-03-29 22:36:54 +0900

    섹션 6. 주문 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,8 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Order {
     @Id @GeneratedValue
     @Column(name = "order_id")
@@ -50,5 +54,41 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직 ==//
+    //1. 주문취소
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소 불가");
+        }
+
+        this.setStatus(OrderStatus.CANCLE);
+        for(OrderItem orderItem : orderItems) {
+            orderItem.cancel(); //한번 주문할 때 상품 두개 주문할수있음, 취소할때 각각에 cancel날림
+        }
+    }
+
+    //==조회 로직 ==//
+    //2. 전체 주문 가격 조회
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+
+    }
 
 }
